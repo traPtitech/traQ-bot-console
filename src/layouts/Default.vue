7f45a63b0b638def82382df02214a1973343c112
takashi <t.k.mk3.110@gmail.com>
2019-04-02 17:31:59 +0900

    quasar

---
+++ src/layouts/Default.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+
+    <q-header class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          traQ BOT Console
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="left" side="left">
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDefault',
+  data () {
+    return {
+      left: true
+    }
+  }
+}
+</script>
