dad691380de95b1bcae33303df0132d39bd99afa
takashi <t.k.mk3.110@gmail.com>
2019-04-04 11:22:21 +0900

    fix delete

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ export function setAuthToken (token) {
   if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
   } else {
-    axios.defaults.headers.common.delete('Authorization')
+    delete axios.defaults.headers.common['Authorization']
   }
 }
 
