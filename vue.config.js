a6cc7b49ede4694a3657929a956e229686b315f3
takashi <t.k.mk3.110@gmail.com>
2019-05-13 12:42:16 +0900

    アイコン画像(made by @unipota)追加

--- vue.config.js
+++ vue.config.js
@@ -54,6 +54,15 @@ module.exports = {
     workboxOptions: {
       skipWaiting: true,
       clientsClaim: true
+    },
+    themeColor: '#ffffff',
+    msTileColor: '#da532c',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png'
     }
   }
 }
