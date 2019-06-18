1ba069a9e73ea992f145f52a7ad5d58381565e7f
sapphi-red <green@sapphi.red>
2019-06-18 15:17:57 +0900

    client secretの表示

--- src/views/ClientDetail.vue
+++ src/views/ClientDetail.vue
@@ -2,21 +2,36 @@
   q-page.q-pa-md.q-gutter-md
     template(v-if="client !== null")
       h6 {{ client.name }} Clientの詳細
-      q-form.col(@submit="onSubmit")
-        q-input(label="Client ID" v-model="client.clientId" readonly hint='')
-        q-input(label="Client名" stack-label v-model="name.value" :readonly="!editing" :counter="editing" maxlength="32" :rules="[val => val && val.length > 0 || '必須項目です']")
-        q-input(label="説明" stack-label v-model="description.value" :readonly="!editing" type="textarea" autogrow :rules="[val => val && val.length > 0 || '必須項目です']")
-        q-input(label="リダイレクト先URL" stack-label v-model="redirectUrl.value" :readonly="!editing" :counter="editing" :rules="[val => val && urlRegex.test(val) || '有効なURLを入力してください']")
-        q-field(label="権限" stack-label :value="client.scopes" readonly hint='')
-          template(v-slot:control)
-            q-option-group(v-model="client.scopes" :options="scopeOptions" type="checkbox" readonly hint='')
-        q-input(v-if="client.creatorId !== userInfo.userId" label="作成者" hint='' v-model="client.creatorName" readonly)
-        div.row.q-gutter-sm(v-if="editing")
-          q-btn.col.btn-fixed-width(label="キャンセル" unelevated @click="cancelEditing")
-          q-btn.col.btn-fixed-width(label="送信" color="primary" type="submit" unelevated)
-      div.row.q-gutter-sm(v-if="!editing")
-        q-btn.col.btn-fixed-width(color="secondary" unelevated @click="startEditing") 編集
-        q-btn.col.btn-fixed-width(color="negative" unelevated @click="onDeleteBtnClicked") 削除
+      div.q-gutter-md
+        div.row.q-col-gutter-md
+          div.col-12
+            q-tabs.text-grey(v-model="tab" dense active-color="primary" indicator-color="primary" narrow-indicator align="left")
+              q-tab(name="info" label="基本情報")
+              q-tab(name="cred" label="認証情報")
+            q-separator
+            q-tab-panels(v-model="tab" animated)
+              q-tab-panel(name="info")
+                q-form.col(@submit="onSubmit")
+                  q-input(label="Client ID" v-model="client.clientId" readonly hint='')
+                  q-input(label="Client名" stack-label v-model="name.value" :readonly="!editing" :counter="editing" maxlength="32" :rules="[val => val && val.length > 0 || '必須項目です']")
+                  q-input(label="説明" stack-label v-model="description.value" :readonly="!editing" type="textarea" autogrow :rules="[val => val && val.length > 0 || '必須項目です']")
+                  q-input(label="リダイレクト先URL" stack-label v-model="redirectUrl.value" :readonly="!editing" :counter="editing" :rules="[val => val && urlRegex.test(val) || '有効なURLを入力してください']")
+                  q-field(label="権限" stack-label :value="client.scopes" readonly hint='')
+                    template(v-slot:control)
+                      q-option-group(v-model="client.scopes" :options="scopeOptions" type="checkbox" readonly hint='')
+                  q-input(v-if="client.creatorId !== userInfo.userId" label="作成者" hint='' v-model="client.creatorName" readonly)
+                  div.row.q-gutter-sm(v-if="editing")
+                    q-btn.col.btn-fixed-width(label="キャンセル" unelevated @click="cancelEditing")
+                    q-btn.col.btn-fixed-width(label="送信" color="primary" type="submit" unelevated)
+                div.row.q-gutter-sm(v-if="!editing")
+                  q-btn.col.btn-fixed-width(color="secondary" unelevated @click="startEditing") 編集
+                  q-btn.col.btn-fixed-width(color="negative" unelevated @click="onDeleteBtnClicked") 削除
+              q-tab-panel(name="cred")
+                p 以下の認証情報の取り扱いに十分注意してください
+                q-form
+                  q-input(label="Client Secret" :value="client.secret" :type="hideSecret ? 'password' : 'text'")
+                    template(slot="append")
+                      q-icon(:name="hideSecret ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="hideSecret = !hideSecret")
 
     template(v-else)
       span 読み込み中...
@@ -32,6 +47,7 @@ export default {
   data () {
     return {
       loading: false,
+      tab: 'info',
       client: null,
       editing: false,
       editingIcon: false,
@@ -48,7 +64,8 @@ export default {
         temp: ''
       },
       scopeOptions,
-      urlRegex: /http(s)?:\/\/([\w-]+.)+[\w-]+(\/[\w- ./?%&=]*)?/i
+      urlRegex: /http(s)?:\/\/([\w-]+.)+[\w-]+(\/[\w- ./?%&=]*)?/i,
+      hideSecret: true
     }
   },
   computed: {
