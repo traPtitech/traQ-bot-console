a226684af4028252eb40c4dd2c5278ee6dc2604c
Pugma <pugmatech@gmail.com>
2026-07-01 16:22:05 +0900

    chore(lint): strengthen TypeScript rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,37 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import pluginVue from 'eslint-plugin-vue'
+
+const commonRules = {
+  'vue/custom-event-name-casing': 'error',
+  'vue/multi-word-component-names': 'off',
+  '@typescript-eslint/ban-ts-comment': [
+    'error',
+    {
+      'ts-check': false,
+      'ts-expect-error': 'allow-with-description',
+      'ts-ignore': true,
+      'ts-nocheck': true,
+      minimumDescriptionLength: 3,
+    },
+  ],
+  '@typescript-eslint/consistent-type-imports': [
+    'error',
+    {
+      fixStyle: 'separate-type-imports',
+      prefer: 'type-imports',
+    },
+  ],
+  '@typescript-eslint/no-unused-vars': [
+    'error',
+    {
+      argsIgnorePattern: '^_',
+      caughtErrors: 'none',
+      ignoreRestSiblings: true,
+      varsIgnorePattern: '^_',
+    },
+  ],
+}
 
 export default [
   ...pluginVue.configs['flat/recommended'],
@@ -12,9 +44,11 @@ export default [
       },
     },
     files: ['src/**/*.ts'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
     rules: {
-      'vue/custom-event-name-casing': 'error',
-      'vue/multi-word-component-names': 'off',
+      ...commonRules,
     },
   },
   {
@@ -28,9 +62,25 @@ export default [
       },
     },
     files: ['src/**/*.vue'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
     rules: {
-      'vue/custom-event-name-casing': 'error',
-      'vue/multi-word-component-names': 'off',
+      ...commonRules,
+    },
+  },
+  {
+    files: [
+      'src/components/**/*.{ts,vue}',
+      'src/views/Home.vue',
+      'src/api.ts',
+      'src/utils.ts',
+      'src/permissions.ts',
+      'src/router/**/*.ts',
+      'src/store/**/*.ts',
+    ],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
     },
   },
 ]
